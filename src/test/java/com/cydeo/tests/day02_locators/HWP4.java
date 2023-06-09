3babe11e96655761c6ea5628ad8601d3f3f13e26
AndrewBalint51 <andrew.balint@hotmail.com>
2023-06-09 16:51:50 -0700

    Finished HWP4

---
+++ src/test/java/com/cydeo/tests/day02_locators/HWP4.java
@@ -0,0 +1,43 @@
+package com.cydeo.tests.day02_locators;
+
+import com.cydeo.tests.Utilities.WebDriverUtility;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HWP4 {
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverUtility.openChrome();
+
+        //2- Go to: https://practice.cydeo.com/inputs
+        driver.get("https://practice.cydeo.com/inputs");
+
+        //3- Click to “Home” link
+        WebElement homeButton = driver.findElement(By.className("nav-link"));
+        homeButton.click();
+
+        String expectedTitle = "Practice";
+        String actualTitle = driver.getTitle();
+
+        if(actualTitle.equals(expectedTitle)){
+            System.out.println("PASSED");
+        }
+        else{
+            System.out.println("FAILED");
+        }
+
+
+
+    }
+
+    /*
+    TC #4: Practice Cydeo – Class locator practice
+1- Open a chrome browser
+2- Go to: https://practice.cydeo.com/inputs
+3- Click to “Home” link
+4- Verify title is as expected:
+Expected: Practice
+PS: Locate “Home” link using “className” locator
+     */
+}
