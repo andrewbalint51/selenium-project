7b896939617ffd0da8ae0ffd6bdf1a1b5f553a46
AndrewBalint51 <andrew.balint@hotmail.com>
2023-06-12 22:47:43 -0700

    practices for day 4

---
+++ src/test/java/com/cydeo/tests/day05_testNG/TC1_StaleElementException.java
@@ -0,0 +1,51 @@
+package com.cydeo.tests.day05_testNG;
+
+import com.cydeo.tests.Utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.time.Duration;
+
+public class TC1_StaleElementException {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        driver.get("https://practice.cydeo.com/add_remove_elements/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebElement addElementButton = driver.findElement(By.xpath("//button[.='Add Element']"));
+
+        addElementButton.click();
+
+        WebElement deleteButton = driver.findElement(By.xpath("//button[.='Delete']"));
+
+        System.out.println("deleteButton.isDisplayed(), expecting true = " + deleteButton.isDisplayed());
+
+        Thread.sleep(2000);
+        deleteButton.click();
+
+        try {
+            System.out.println("deleteButton.isDisplayed(), expecting false = " + deleteButton.isDisplayed());
+        }
+        catch (StaleElementReferenceException e){
+            System.out.println("deleteButton.isDisplayed(), expecting false = false");
+        }
+
+
+        driver.close();
+    }
+    /*
+    TC #1: StaleElementReferenceException handling
+1. Open Chrome browser
+2. Go to https://practice.cydeo.com/add_remove_elements/
+3. Click to “Add Element” button
+4. Verify “Delete” button is displayed after clicking.
+5. Click to “Delete” button.
+6. Verify “Delete” button is NOT displayed after clicking.
+USE XPATH LOCATOR FOR ALL WEBELEMENT LOCATORS
+     */
+}
---
+++ src/test/java/com/cydeo/tests/day05_testNG/TC3_UtilityMethod.java
@@ -0,0 +1,59 @@
+package com.cydeo.tests.day05_testNG;
+
+import com.cydeo.tests.Utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.time.Duration;
+import java.util.List;
+
+public class TC3_UtilityMethod {
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        driver.get("https://practice.cydeo.com/radio_buttons");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        clickAndVerifyRadioButton(driver, "color", "yellow");
+
+        driver.close();
+    }
+
+    public static void clickAndVerifyRadioButton(WebDriver driver, String name, String ID){
+
+        List<WebElement> radioButtons = driver.findElements(By.xpath("//input[@name='"+name+"']"));
+
+        for (WebElement each : radioButtons) {
+            if(each.getAttribute("id").equals(ID)){
+                each.click();
+                System.out.println(ID+" is selected: "+each.isSelected());
+                break;
+            }
+        }
+
+        //System.out.println("hockeyRadioButton.isSelected(), expecting true = " + hockeyRadioButton.isSelected());
+
+    }
+
+    /*
+    TC #3: Utility method task for (continuation of Task2)
+1. Open Chrome browser
+2. Go to https://practice.cydeo.com/radio_buttons
+3. Click to “Hockey” radio button
+4. Verify “Hockey” radio button is selected after clicking.
+
+USE XPATH LOCATOR FOR ALL WEBELEMENT LOCATORS
+Create a utility method to handle above logic.
+Method name: clickAndVerifyRadioButton
+Return type: void or boolean
+Method args:
+1. WebDriver
+2. Name attribute as String (for providing which group of radio buttons)
+3. Id attribute as String (for providing which radio button to be clicked)
+Method should loop through the given group of radio buttons. When it finds the
+matching option, it should click and verify option is Selected.
+Print out verification: true
+     */
+}
